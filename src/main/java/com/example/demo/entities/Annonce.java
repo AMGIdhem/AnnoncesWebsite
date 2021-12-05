86898cb7058f5d39e3a779d305f94859208d367b
Mahdi Bouhouch <47068558+AMGIdhem@users.noreply.github.com>
2021-12-05 16:28:31 +0000

    Update Annonce.java

--- src/main/java/com/example/demo/entities/Annonce.java
+++ src/main/java/com/example/demo/entities/Annonce.java
@@ -12,7 +12,7 @@ import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
-
+//comment
 @Entity
 public class Annonce {
 	@Id
